92969abe56a571a7e64e96c1958f3f384cf49242
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-04-10 14:36:09 +0200

    FUTURE
    Using the doFuture for paralleization

--- R/Pattern_reconstruction.R
+++ R/Pattern_reconstruction.R
@@ -19,96 +19,42 @@
 #' @export
 Pattern.Reconstruction <- function(pattern, method="only_spatial",
                                    number_reconstructions=1, max_runs=10000, e_threshold=0.01,
-                                   fitting=F, parallel=F, verbose=T){
+                                   fitting=F, verbose=F){
 
-  result <- list()
+  doFuture::registerDoFuture()
+  future::plan(multisession)
 
-  if(parallel==T){
-    if(Sys.info()[[1]]=="Windows" || Sys.info()[[1]]=="Darwin"){cores <- parallel::detectCores()/2}
-    else{cores <- parallel::detectCores()}
-
-    if(verbose==T){
-      print(paste0("Parallel computation using: ", cores, " cores"))
-      pb <- utils::txtProgressBar(max=number_reconstructions, style=3)
-      progress <- function(n) utils::setTxtProgressBar(pb, n)
-      opts <- list(progress=progress)
+  if(method=="only_spatial"){
+    result <- foreach::foreach(i=1:number_reconstructions)%dorng%{
+      SHAR::Spatial.Reconstruction(pattern=pattern, max_runs=max_runs,
+                                   e_threshold=e_threshold, fitting=fitting, verbose=verbose)
     }
-    else{opts<-list()}
+  }
 
-    cl <- parallel::makeCluster(cores)
-    doSNOW::registerDoSNOW(cl)
-
-    if(method=="only_spatial"){
-      result <- foreach::foreach(i=1:number_reconstructions, .options.snow=opts)%dorng%{
-        SHAR::Spatial.Reconstruction(pattern=pattern, max_runs=max_runs,
-                               e_threshold=e_threshold, fitting=fitting, verbose=F)
-      }
+  else if(method=="splitting_species"){
+    result <- foreach::foreach(i=1:number_reconstructions)%dorng%{
+      SHAR::Splitting.Reconstruction(pattern=pattern, max_runs=max_runs,
+                                     e_threshold=e_threshold, fitting=fitting, verbose=verbose)
     }
+  }
 
-    else if(method=="splitting_species"){
-      result <- foreach::foreach(i=1:number_reconstructions, .options.snow=opts)%dorng%{
-        SHAR::Splitting.Reconstruction(pattern=pattern, max_runs=max_runs,
-                               e_threshold=e_threshold, fitting=fitting, verbose=F)
-      }
+  else if(method=="random_labeling"){
+    result <- foreach::foreach(i=1:number_reconstructions)%dorng%{
+      SHAR:: Labeling.Reconstruction(pattern=pattern, max_runs=max_runs,
+                                     e_threshold=e_threshold, fitting=fitting, verbose=verbose)
     }
+  }
 
-    else if(method=="random_labeling"){
-      result <- foreach::foreach(i=1:number_reconstructions, .options.snow=opts)%dorng%{
-       SHAR:: Labeling.Reconstruction(pattern=pattern, max_runs=max_runs,
-                               e_threshold=e_threshold, fitting=fitting, verbose=F)
-      }
+  else if(method=="simultaneously"){
+    result <- foreach::foreach(i=1:number_reconstructions)%dorng%{
+      SHAR::Simultaneously.Reconstruction(pattern=pattern, max_runs=max_runs,
+                                          e_threshold=e_threshold, fitting=fitting, verbose=verbose)
     }
-
-    else if(method=="simultaneously"){
-      result <- foreach::foreach(i=1:number_reconstructions, .options.snow=opts)%dorng%{
-        SHAR::Simultaneously.Reconstruction(pattern=pattern, max_runs=max_runs,
-                               e_threshold=e_threshold, fitting=fitting, verbose=F)
-      }
-    }
-
-    else{
-      print("Please select either 'only_spatial', 'splitting_species', 'random_labeling' or 'simultaneously' as method")
-      result[[1]] <- "NA"
-    }
-
-    if(verbose==T){close(pb)}
-    parallel::stopCluster(cl)
   }
 
   else{
-    if(verbose==T){
-      print(paste0("Non-parallel computation"))
-      pb <- utils::txtProgressBar(max=number_reconstructions, style=3)
-      }
-
-    for(i in 1:number_reconstructions){
-      if(method=="only_spatial"){
-        result[[i]] <- SHAR::Spatial.Reconstruction(pattern=pattern, max_runs=max_runs,
-                                              e_threshold=e_threshold, fitting=fitting, verbose=F)
-      }
-
-      else if(method=="splitting_species"){
-        result[[i]] <- SHAR::Splitting.Reconstruction(pattern=pattern, max_runs=max_runs,
-                                                e_threshold=e_threshold, fitting=fitting, verbose=F)
-      }
-
-      else if(method=="random_labeling"){
-        result[[i]] <- SHAR::Labeling.Reconstruction(pattern=pattern, max_runs=max_runs,
-                                               e_threshold=e_threshold, fitting=fitting, verbose=F)
-      }
-
-      else if(method=="simultaneously"){
-        result[[i]] <- SHAR::Simultaneously.Reconstruction(pattern=pattern, max_runs=max_runs,
-                                                     e_threshold=e_threshold, fitting=fitting, verbose=F)
-      }
-
-      else{
-        print("Please select either 'only_spatial', 'splitting_species', 'random_labeling' or 'simultaneously' as method")
-        result[[1]] <- "NA"
-      }
-      if(verbose==T){utils::setTxtProgressBar(pb, i)}
-    }
-    if(verbose==T){close(pb)}
+    print("Please select either 'only_spatial', 'splitting_species', 'random_labeling' or 'simultaneously' as method")
+    result <- list("NA")
   }
 
   if(method=="only_spatial"){result[[length(result)+1]] <- spatstat::unmark(pattern)}
