ceb1c29e6fa34b2fd70b4007de53f8f167775af3
marcosci <sciaini.marco@gmail.com>
2018-05-04 16:01:49 +0200

    remove NOTES
    created SHAR.R with global variables, put spatstat.data in suggests. spammed Rbuildignore

---
+++ R/SHAR.R
@@ -0,0 +1,31 @@
+#' SHAR
+#'
+#' \emph{SHAR} provides ,,,
+#'
+#' @importFrom stats na.omit runif setNames
+
+# nocov start
+# nolint start
+"_PACKAGE"
+
+globalVariables(c("Species",
+                  "Type",
+                  "pcf",
+                  "lo",
+                  "hi",
+                  "gr",
+                  "Habitat",
+                  ".",
+                  "Correct",
+                  "False",
+                  "r",
+                  "theo",
+                  "rs",
+                  "iso",
+                  "Mean",
+                  "Count"))
+
+# nolint end
+# nocov end
+
+
