7db2149719de5d5c646cf1e52ffda71309677b94
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-05-07 16:18:34 +0200

    Rename .R files style code

---
+++ R/reconstruct_labeling.R
@@ -0,0 +1,64 @@
+#' Pattern reconstruction
+#'
+#' Multivariate pattern reconstruction using a random marking approach
+#' @param pattern [\code{ppp(1)}] Multivariate ppp object of the spatstat package
+#' @param max_runs [\code{numeric(1)}] number of maximum iterations
+#' @param e_threshold [\code{numeric(1)}] Threshold for energy to reach during reconstruction
+#' @param fitting [\code{logical(1)}] If TRUE, a clustered pattern is fitted to the original pattern as starting point
+#'
+#' @return ppp object of the spatstat package with reconstructed pattern
+
+#' @export
+reconstruct_labeling <- function(pattern, max_runs = 10000, e_threshold = 0.01, fitting = FALSE){
+
+  simulated <- SHAR::reconstruct_spatial(pattern = pattern, max_runs = max_runs,
+                                         e_threshold = e_threshold,
+                                         fitting = fitting)
+
+  pattern <- Select.Species(pattern) # get data with only species as marks
+
+  species <- rep(levels(pattern$marks), spatstat::summary.ppp(pattern)$marks[[1]]) # create marks
+  spatstat::marks(simulated) <- factor(species) # assign marks to data
+
+  pcfmulti_observed <- SHAR::Pcf.Multi(pattern) # iSAR observed data
+  pcfmulti_simulated <- SHAR::Pcf.Multi(simulated)  # iSAR simulated data
+
+  gmulti_observed <- SHAR::Gest.Multi(pattern) # Gmulti(r) observed data
+  gmulti_simulated <- SHAR::Gest.Multi(simulated) # Gmulti(r) simulated data
+
+  e0_pcfmulti <- mean(abs(pcfmulti_observed$Mean - pcfmulti_simulated$Mean), na.rm = TRUE) # energy iSAR
+  e0_gmulti <- mean(abs(gmulti_observed$Mean - gmulti_simulated$Mean), na.rm = TRUE) # energy Gmulti
+
+  e0_spec <- e0_pcfmulti + e0_gmulti # overall energy
+
+  for(i in 1:max_runs){ # mark reconstruction
+    relocated <- simulated # create relocated data
+
+    sample_species <- sample(unique(species), size = 2)
+
+    point1 <- sample(size=1, 1:relocated[relocated$marks == sample_species[1]]$n) # random point 1
+    point2 <- sample(size=1, 1:relocated[relocated$marks == sample_species[2]]$n) # random point 2 (datset without spec1)
+
+    spatstat::marks(relocated[relocated$marks == sample_species[1]][point1]) <- sample_species[2] # swap species
+    spatstat::marks(relocated[relocated$marks == sample_species[2]][point2]) <- sample_species[1] # swap species
+
+    pcfmulti_relocated <- SHAR::Pcf.Multi(relocated) # iSAR after relocation
+    gmulti_relocated <- SHAR::Gest.Multi(relocated) # Gmulti(r) after relocation
+
+    e_relocated_pcfmulti <- mean(abs(pcfmulti_observed$Mean - pcfmulti_relocated$Mean), na.rm = TRUE) # energy iSAR after relocation
+    e_relocated_gmulti <- mean(abs(gmulti_observed$Mean - gmulti_relocated$Mean), na.rm = TRUE) # energy Gmulti(r) after relocation
+
+    e_relocated_spec <- e_relocated_pcfmulti + e_relocated_gmulti # overall energy after relocation
+
+    if(e_relocated_spec<e0_spec){ # lower energy after relocation
+      simulated <- relocated # keep relocated data
+      e0_spec <- e_relocated_spec # keep energy
+      pcfmulti_simulated <- pcfmulti_relocated # keep iSAR
+      gmulti_simulated <- gmulti_relocated # keep Gmulti(r)
+    }
+
+    if(e0_spec<=e_threshold){break} # exit loop
+  }
+
+  return(simulated) # return results
+}
---
+++ R/reconstruct_simultaneously.R
@@ -0,0 +1,105 @@
+#' Pattern reconstruction
+#'
+#' Multivariate pattern reconstruction using a simultaneously approach
+#' @param pattern [\code{ppp(1)}] Multivariate ppp object of the spatstat package
+#' @param max_runs [\code{numeric(1)}] number of maximum iterations
+#' @param e_threshold [\code{numeric(1)}] Threshold for energy to reach during reconstruction
+#' @param fitting [\code{logical(1)}] If TRUE, a clustered pattern is fitted to the original pattern as starting point
+#'
+#' @return ppp object of the spatstat package with reconstructed pattern
+
+#' @export
+reconstruct_simultaneously <- function(pattern, max_runs = 10000, e_threshold = 0.01, fitting = FALSE){
+
+  pattern <- Select.Species(pattern) # data to reconstruct
+
+  if(fitting == TRUE){
+    simulated <- pattern %>%
+      spatstat::unmark() %>%
+      spatstat::kppm(cluster='Thomas', statistic = 'pcf') %>%
+      spatstat::simulate.kppm(window = pattern$window, drop = TRUE, verbose = FALSE)
+
+    if(simulated$n != pattern$n){
+      dif <- abs(pattern$n - simulated$n)
+      if(simulated$n < pattern$n){
+        p <- spatstat::runifpoint(n = dif, win = pattern$window)
+        simulated <- spatstat::superimpose(simulated, p)
+      }
+      else{
+        r <- sample(1:max(simulated$n), size = dif, replace = FALSE)
+        simulated <- simulated[-r]
+      }
+    }
+  }
+  else{simulated <- spatstat::runifpoint(n = spatstat::unmark(pattern)$n,
+                                         win = spatstat::unmark(pattern)$window)} # create simulation data
+
+  species <- rep(levels(pattern$marks), spatstat::summary.ppp(pattern)$marks[[1]]) # get marks
+  spatstat::marks(simulated) <- factor(species) # assign marks to simulated pattern
+
+  if(pattern$n >= 1000){ # indirect computation
+    pcf_observed <- SHAR::Pcf.Fast(pattern)
+    pcf_simulated <- SHAR::Pcf.Fast(simulated)
+  }
+
+  else{ # direct computation
+    pcf_observed <- spatstat::pcf(pattern, correction = 'best', divisor = 'd') # g(r) observed data
+    pcf_simulated <- spatstat::pcf(simulated, correction = 'best', divisor = 'd') # g(r) simulated data
+  }
+
+  gest_observed <- spatstat::Gest(pattern, correction = 'best') # G(r) observed data
+  gest_simulated <- spatstat::Gest(simulated, correction = 'best') # g(r) simulated pattern
+
+  pcfmulti_observed <- SHAR::Pcf.Multi(pattern) # iSAR observed data
+  pcfmulti_simulated <- SHAR::Pcf.Multi(simulated) # iSAR simulated data
+
+  gmulti_observed <- SHAR::Gest.Multi(pattern) # Gmulti(r) observed data
+  gmulti_simulated <- SHAR::Gest.Multi(simulated) # Gmulti(r) simulated data
+
+  e0_pcf <- mean(abs(pcf_observed[[3]] - pcf_simulated[[3]]), na.rm = TRUE) # energy g(r)
+  e0_gest <- mean(abs(gest_observed[[3]] - gest_simulated[[3]]), na.rm = TRUE) # energy G(r)
+  e0_pcfmulti <- mean(abs(pcfmulti_observed$Mean - pcfmulti_simulated$Mean), na.rm = TRUE) # energy isar
+  e0_gmulti <- mean(abs(gmulti_observed$Mean - gmulti_simulated$Mean), na.rm = TRUE) # energy Gmulti
+
+  e0 <- e0_pcf + e0_gest + e0_pcfmulti + e0_gmulti # overall energy
+
+  for(i in 1:max_runs){ # pattern reconstruction
+    relocated <- simulated # create relocation data
+
+    rp <- sample(size=1, 1:relocated$n) # random point of pattern
+    relocated_temp <- relocated[-rp] # remove point from pattern
+    point <- spatstat::runifpoint(n = 1, win = relocated$window) # create random coordinates
+    spatstat::marks(point) <- spatstat::marks(relocated[rp]) # assign marks
+    relocated <- spatstat::superimpose(relocated_temp, point) # add point to pattern
+
+    if(relocated$n >= 1000){ # indirect computation
+      k_relocated <- spatstat::Kest(relocated, correction = 'good') # K(r) after relocation
+      pcf_relocated <- spatstat::pcf.fv(k_relocated, spar = 0.5, method = 'd') # g(r) after relocation
+    }
+    else{pcf_relocated <- spatstat::pcf(relocated, correction = 'best', divisor = 'd')} # g(r) after relocation
+
+    gest_relocated <- spatstat::Gest(relocated, correction = 'best') # G(r) after relocation
+    pcfmulti_relocated <- SHAR::Pcf.Multi(relocated) # iSAR after relocation
+    gmulti_relocated <- SHAR::Gest.Multi(relocated) # Gmulti(r) simulated data
+
+    e_relocated_pcf <- mean(abs(pcf_observed[[3]] - pcf_relocated[[3]]), na.rm = TRUE) # energy g(r) after relocation
+    e_relocated_gest <- mean(abs(gest_observed[[3]] - gest_relocated[[3]]), na.rm = TRUE) # energy G(r) after relocation
+    e_relocated_pcfmulti <- mean(abs(pcfmulti_observed$Mean - pcfmulti_relocated$Mean), na.rm = TRUE) # energy iSAR after relocation
+    e_relocated_gmulti <- mean(abs(gmulti_observed$Mean - gmulti_relocated$Mean), na.rm = TRUE) # energy Gmulti
+
+    e_relocated <- e_relocated_pcf + e_relocated_gest + e_relocated_pcfmulti + e_relocated_gmulti # total energy after relocation
+
+    if(e_relocated<e0){ # lower energy after relocation
+      simulated <- relocated # keep relocated pattern
+      e0 <- e_relocated # keep e_relocated
+      pcf_simulated <- pcf_relocated # keep pcf_relocated
+      gest_simulated <- gest_relocated # keep Gest_relocated
+      pcfmulti_simulated <- pcfmulti_relocated # keep iSAR
+      gmulti_simulated <- gmulti_relocated # keep Gmulti(r)
+    }
+
+    if(e0<=e_threshold){break}
+  }
+
+  return(simulated) # return results
+}
