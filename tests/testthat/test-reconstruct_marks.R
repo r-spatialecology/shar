3d297b4a41bf525e9eb794b20aceef4d77a3df1a
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-12-20 15:38:31 +0100

    Ups...forgot one test

--- tests/testthat/test-reconstruct_marks.R
+++ tests/testthat/test-reconstruct_marks.R
@@ -34,6 +34,28 @@ test_that("Input pattern can not be returned for reconstruct_marks", {
   expect_false(any(marks_sub %in% marks_recon))
 })
 
+test_that("Only pattern can be returned for simplify = TRUE", {
+
+  marks_recon <- SHAR::reconstruct_marks(pattern = pattern_recon,
+                                         marked_pattern = marks_sub,
+                                         n_random = 1, max_runs = 10,
+                                         return_input = FALSE, simplify = TRUE)
+
+  expect_is(marks_recon, "ppp")
+})
+
+test_that("All optional arguments can be used for reconstruct_marks", {
+
+  marks_recon <- SHAR::reconstruct_marks(pattern = pattern_recon,
+                                         marked_pattern = marks_sub,
+                                         n_random = 3, max_runs = 10,
+                                         plot = TRUE)
+
+
+  expect_type(marks_recon, type = "list")
+  expect_length(marks_recon, n = 4)
+})
+
 test_that("All errors are returned for reconstruct_marks", {
 
   expect_error(SHAR::reconstruct_marks(pattern = pattern_recon,
@@ -61,16 +83,6 @@ test_that("All errors are returned for reconstruct_marks", {
                                        marked_pattern = spatstat::subset.ppp(species_a, select = status),
                                        n_random = 3, max_runs = 10),
                regexp = "marks must be 'numeric'")
-})
-
-test_that("Only pattern can be returned for simplify = TRUE", {
-
-  marks_recon <- SHAR::reconstruct_marks(pattern = pattern_recon,
-                                         marked_pattern = marks_sub,
-                                         n_random = 1, max_runs = 10,
-                                         return_input = FALSE, simplify = TRUE)
-
-  expect_is(marks_recon, "ppp")
 })
 
 test_that("All warnings are returned for reconstruct_marks", {
