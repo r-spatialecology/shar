670dd7d742a31524b84685ddc09506821e32f119
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2019-03-25 16:13:30 +0100

    Update test

--- tests/testthat/test-estimate_pcf_fast.R
+++ tests/testthat/test-estimate_pcf_fast.R
@@ -1,7 +1,8 @@
 testthat::context("test-estimate_pcf_fast")
 
-pcf <- shar::estimate_pcf_fast(pattern = shar::species_b)
-
 testthat::test_that("estimate_pcf returns spatstat.fv object", {
-  testthat::expect_is(pcf, "fv")
+
+  pcf_est <- shar::estimate_pcf_fast(pattern = shar::species_b)
+
+  testthat::expect_is(pcf_est, "fv")
 })
