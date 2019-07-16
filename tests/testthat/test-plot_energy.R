04df51dbaefd51c800d6c5af2270e2b25598a334
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2019-07-16 16:18:43 +0200

    :white_check_mark: Update tests

--- tests/testthat/test-plot_energy.R
+++ tests/testthat/test-plot_energy.R
@@ -17,3 +17,10 @@ testthat::test_that("plot_energy uses colours", {
   testthat::expect_null(shar::plot_energy(pattern_reconstruction,
                                           col = c("blue", "green", "red")))
 })
+
+testthat::test_that("plot_energy returns error", {
+
+  testthat::expect_error(shar::plot_energy(pattern = 1:10),
+                         regexp = "Class of 'pattern' must be 'rd_pat' or 'rd_mar'.",
+                         fixed = TRUE)
+})
---
+++ tests/testthat/test-print.rd_pat.R
@@ -0,0 +1,43 @@
+testthat::context("test-print.rd_pat")
+
+pattern_random <- shar::fit_point_process(pattern = shar::species_a,
+                                          n_random = 3,
+                                          verbose = FALSE)
+
+energy_random <- round(shar::calculate_energy(pattern_random,
+                                        return_mean = TRUE,
+                                        verbose = FALSE),
+                       digits = 4)
+
+pattern_recon <- shar::reconstruct_pattern(pattern = shar::species_a,
+                                           n_random = 3,
+                                           max_runs = 10,
+                                           verbose = FALSE)
+
+energy_recon <- round(shar::calculate_energy(pattern_recon,
+                                             return_mean = TRUE,
+                                             verbose = FALSE),
+                       digits = 4)
+
+################################################################################
+
+testthat::test_that("print.rd_pat works for fitted pattern", {
+
+  output <- testthat::capture_output(shar::print.rd_pat(pattern_random))
+
+  testthat::expect_equal(object = output,
+                         expected = paste0("No. of pattern: 4\nMean energy: ",
+                                           energy_random,
+                                           "\nMethod: fit_point_process()\nObserved pattern: included\nIterations (mean): NA"))
+})
+
+
+testthat::test_that("print.rd_pat works for reconstructed pattern", {
+
+  output <- testthat::capture_output(shar::print.rd_pat(pattern_recon))
+
+  testthat::expect_equal(object = output,
+                         expected = paste0("No. of pattern: 4\nMean energy: ",
+                                           energy_recon,
+                                           "\nMethod: reconstruct_pattern()\nObserved pattern: included\nIterations (mean): 10"))
+})
