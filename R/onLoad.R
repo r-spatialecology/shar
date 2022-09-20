514b33c276e6cf353bf16f6c8df66ddaadac9221
mhesselbarth <mhk.hesselbarth@gmail.com>
2022-09-20 12:03:43 -0400

    Using http

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   repos = getOption("repos")
-  repos["getCRUCLdata"] = "https://packages.ropensci.org"
+  repos["getCRUCLdata"] = "http://packages.ropensci.org"
   options(repos = repos)
   invisible(repos)
 }
