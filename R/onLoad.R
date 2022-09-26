faebdeb3201dcec92f5d62d6dbe14d76900a5406
mhesselbarth <mhk.hesselbarth@gmail.com>
2022-09-26 07:26:47 -0400

    Adding link onload

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,3 +1,5 @@
+# Source: https://thecoatlessprofessor.com/programming/r/r-data-packages-in-external-data-repositories-using-the-additional_repositories-field/
+
 .onLoad <- function(libname, pkgname) {
 
   repos <- getOption("repos")
