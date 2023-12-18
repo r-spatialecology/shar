68c97824b08f553ca3221bccd2987eb51f1cbd1c
mhesselbarth <mhk.hesselbarth@gmail.com>
2023-12-18 09:28:01 +0100

    Update tests and docs

--- tests/testthat/test-reconstruct_pattern_multi.R
+++ tests/testthat/test-reconstruct_pattern_multi.R
@@ -24,6 +24,10 @@ multi_recon <- reconstruct_pattern_multi(marked_pattern, n_repetitions = 3, max_
 multi_recon_simple <- reconstruct_pattern_multi(marked_pattern, n_repetitions = 1, max_steps = 100,
                                                 verbose = FALSE)
 
+multi_recon_fun <- reconstruct_pattern_multi(marked_pattern, n_repetitions = 1, max_steps = 100,
+                                             verbose = FALSE, w_statistics = c("Dk" = 1, "K" = 0.5, "Hs" = 0.5, "pcf" = 1),
+                                             divisor = "d", kernel = "gaussian")
+
 ################################################################################
 
 testthat::test_that("Output is a long as n_random for reconstruct_pattern_multi", {
@@ -50,4 +54,17 @@ testthat::test_that("Energy decresead for for reconstruct_pattern_multi", {
 
   testthat::expect_lt(object = multi_recon_simple$energy_current,
                       expected = multi_recon_simple$energy_launch)
+})
+
+testthat::test_that("Test additional arguments of reconstruct_pattern_multi", {
+
+  expect_equal(object = multi_recon_fun$Parameter_setting$w_statistics,
+               expected = c("Dk" = 1, "K" = 0.5, "Hs" = 0.5, "pcf" = 1))
+
+  expect_equal(object = multi_recon_fun$Parameter_setting$divisor,
+               expected = "d")
+
+  expect_equal(object = multi_recon_fun$Parameter_setting$kernel_arg,
+               expected = "gaussian")
+
 })
